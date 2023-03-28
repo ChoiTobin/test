711b13df4502a0246f4619b9b9eb860d193fc3ce
Choitobin <rovello2@gmail.com>
2023-03-28 17:29:28 +0900

    push

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -15,65 +15,58 @@ function Map() {
     { img: img5 },
   ];
 
+  // 요구사항  클릭한곳에서 클릭한상태에서 이동한거리 - 전체요소
+  // 드래그를 캐치해서 translate값 100%씩 주기.
+  
+  const scrollRef = useRef(null);
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState();
 
-  // 슬라이더 효과
-  //클릭했을때-땠을떄-움직일때 -마우스이벤트 스크롤 이벤트.
-  //width에 종료 scrollwidth > offsetwidth > clientwidth 크기순서
 
-let onMouseDown = () => {
-  console.log("mouseDown")
+let onDragStart = (e) => {
+  e.preventDefault();
+  setIsDrag(true);
+  setStartX(e.pageX + scrollRef.current.scrollLeft);
+  // e.pageX는 화면상에서 왼쪽 상단 기준으로 마우스 위치의 X축 좌표
+ 
 }
 
-let onMouseUp = () => {
-  console.log("mouseup")
+let onDragEnd = () => {
+  setIsDrag(false);
+
 }
-
-let onMouseMove = () => {
-  console.log("mousemove")
+let onDragMove  = (e) => {
+  if (isDrag) {
+    scrollRef.current.scrollLeft = startX - e.pageX;
+    console.log(scrollRef.current.scrollLeft)
+  }
 }
-
-const DomRef = useRef()
-
-
-
-
-
-useEffect(() => {
-  const wheelHandler = (e) => {
-    e.preventDefault();
-    // 스크롤 행동 구현
-  };
-  const DOMCURRENT = DomRef.current;
-  DOMCURRENT.addEventListener("wheel", wheelHandler);
-  return () => {
-    DOMCURRENT.removeEventListener("wheel", wheelHandler);
-  };
-}, []);
-// 이부분을 휠말고 다른걸로 구현.
-//https://codingbroker.tistory.com/128
-
-
 
   return (
     <>
       <div
-
         className="box0-map"
-        style={{ height: "90vh" }}>
-          {PhotoArr.map((item, i) => (
-              <div
-              ref={DomRef}
-              onMouseDown={onMouseDown}
-              onMouseUp={onMouseUp}
-              onMouseMove={onMouseMove}
-                key={i}
-                className="slide-image"
-                style={{
-                  backgroundImage: `url(${item.img})`,
-                  backgroundSize: "cover",
-                }}
-              ></div>
-          ))}
+        style={{ height: "90vh" }}
+        >
+          <div className="box1-map"
+          ref={scrollRef}
+          >
+                {PhotoArr.map((item, i) => (
+                <div
+                    onMouseDown={onDragStart}
+                    onMouseUp={onDragEnd}
+                    onMouseLeave={onDragEnd}
+                    onMouseMove={onDragMove}
+                    key={i}
+                    className="slide-image"
+                    style={{
+                    transform:`translateX(-0%)`,
+                    backgroundImage: `url(${item.img})`,
+                    backgroundSize: "cover",
+                    }}>
+                </div>
+                ))}
+          </div>
       </div>
     </>
   );
